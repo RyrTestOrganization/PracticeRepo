ddf6ede790cfd98ad1bf08df0040ee0b21068f28
Ryan R <ryr509@yahoo.ca>
2018-03-05 22:05:17 -0500

    other php file

---
+++ output.php
@@ -0,0 +1,9 @@
+<?php
+include_once("hello.php");
+	class Output extends \PHPUnit_Framework_TestCase {
+		public function testString(){
+			$this->student = new Student('Zain Quraishi');
+			$this->assertEquals($this->student->newStudent(), 'Student\'s name is Zain Quraishi' . "\n");
+		}
+	}
+?>
